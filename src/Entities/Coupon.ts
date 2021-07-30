0c959db19a467981ebfdaf0665cc04dbabb44e40
maxi <maxi.rodriguez94@gmail.com>
2021-07-30 10:52:32 -0300

    creation entity cuopons

---
+++ src/Entities/Coupon.ts
@@ -0,0 +1,22 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity({ name: "coupons"})
+export class Coupon {
+    
+    @PrimaryGeneratedColumn()
+    id : Number
+
+    @Column({name : "assigned_at"})
+    asignedAt : Date
+
+    @Column({
+        length : 8
+    })
+    code : String
+
+    @Column({name : "customer_email"})
+    customerEmail : String
+
+    @Column({name : "expires_at"})
+    expireAt : Date
+}
