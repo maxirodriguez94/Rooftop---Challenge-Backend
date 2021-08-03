4ca9c4b8c773c843e2e3cbb8ba89817df0d2409f
maxi <maxi.rodriguez94@gmail.com>
2021-08-03 17:11:06 -0300

    configure findName

--- src/Repositories/StoreRepository.ts
+++ src/Repositories/StoreRepository.ts
@@ -1,10 +1,17 @@
-import {Request, Response} from "express";
+import { Request, Response } from "express";
 import { getRepository, UpdateResult, DeleteResult } from "typeorm";
-import { Store } from "../Entities/Store"
+import { Store } from "../Entities/Store";
 
 export class StoreRepository {
-findAll(): Promise<Store[]> {
+  findAll(): Promise<Store[]> {
     return getRepository(Store).createQueryBuilder("store").getMany();
-}
+  }
 
+  findName(name? : String): Promise<Store> {
+    return getRepository(Store).findOneOrFail({
+      where: {
+        name: name,
+      },
+    });
+  }
 }
