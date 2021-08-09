fc65373aa97d9b1e9a64af46eb321bc33ecf70d8
maxi <maxi.rodriguez94@gmail.com>
2021-08-09 19:37:16 -0300

    configure dotEnv variables

---
+++ ormconfig.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "type": "postgres",
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "schema": process.env.DB_SCHEMA,
+    "synchronize": false,
+    "logging": false,
+    "entities": [
+       "src/Entities/**/*.ts"
+    ],
+    "migrations": [
+       "src/migration/**/*.ts"
+    ],
+    "subscribers": [
+       "src/subscriber/**/*.ts"
+    ]
+ }
+ 
